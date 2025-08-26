2d1b27888b686b67fdb43749bf0d5d46acd45881
Yurii Shkoropad <lvivgeorge@gmail.com>
2025-08-26 22:46:28 +0300

    add thread

--- lora_tunning.py
+++ lora_tunning.py
@@ -1,11 +1,21 @@
 import serial.tools.list_ports
 import logging
+import threading
 
 class LoraTunning:
   def __init__(self, port_filter) -> None:
     self.logger: logging.Logger = self.getLogger()
-    serial_port = self.find_serial_port(port_filter)
-    self.read_serial(serial_port)
+
+    self.ser = None
+    self.thread = None
+    self.running = False
+
+    self.serial_port = self.find_serial_port(port_filter)
+
+    if self.serial_port:
+      self.start_listener()
+    else:
+      self.logger.error("Serial port not found")
 
   def getLogger(self) -> logging.Logger:
     logging.basicConfig(
@@ -22,17 +32,30 @@ class LoraTunning:
         return port.device
     return None
   
-  def read_serial(self, port: str):
-    ser = serial.Serial(port=port, baudrate=9600, timeout=1)
-
-    self.logger.info(f"Connected to {port}")
+  def start_listener(self):
+    self.ser = serial.Serial(port=self.serial_port, baudrate=9600, timeout=1)
+    self.running = True
+    self.thread = threading.Thread(target=self.read_serial, daemon=True)
+    self.thread.start()
+    self.logger.info(f"Listening thread started on {self.serial_port}")
+  
+  def read_serial(self):
+    self.logger.info(f"Connected to {self.serial_port}")
 
     try:
       while True:
-        if ser.in_waiting > 0:
-          line = ser.readline().decode('utf-8', errors='ignore').strip()
-          self.logger.info("Received:", line)
-    except KeyboardInterrupt:
-      self.logger.info("Done")
+        if self.ser.in_waiting > 0:
+          line = self.ser.readline().decode('utf-8', errors='ignore').strip()
+          if line:
+            self.logger.info("Received:", line)
+    except Exception as e:
+      self.logger.error(f"Error in read_serial: {e}")
     finally:
-      ser.close()
+      self.ser.close()
+      self.logger.info("Serial connection closed")
+
+  def stop_listener(self):
+    self.running = False
+    if self.thread and self.thread.is_alive():
+      self.thread.join(timeout=2)
+    self.logger.info("Listener stopped")
--- main.py
+++ main.py
@@ -6,11 +6,13 @@ load_dotenv(override=True)
 
 PORT_FILTER = os.getenv('PORT_FILTER')
 
-def main() -> None:
-  LoraTunning(PORT_FILTER)
-
 if __name__ == "__main__":
-    main()
+  loraTunning = LoraTunning(PORT_FILTER)
+  try:
+    while True:
+      pass 
+  except KeyboardInterrupt:
+    loraTunning.stop_listener()
 
 
   
