46a49b5c4564fb014d7623f149f64c10efbb9a7a
Yurii Shkoropad <lvivgeorge@gmail.com>
2025-08-26 22:25:06 +0300

    add LoraTunning

--- main.py
+++ main.py
@@ -1,27 +1,54 @@
 import serial.tools.list_ports
 from dotenv import load_dotenv
 import os
+import logging
 
 load_dotenv(override=True)
 
 PORT_FILTER = os.getenv('PORT_FILTER')
 
-def find_serial_port(filter: str) -> str:
-  ports = serial.tools.list_ports.comports()
-  for port in ports:
-    if port.device.startswith(filter):
-      return port.device
-  return None
+class LoraTunning:
+  def __init__(self) -> None:
+    self.logger: logging.Logger = self.getLogger()
+    serial_port = self.find_serial_port(PORT_FILTER)
+    self.read_serial(serial_port)
 
-def read_serial(port: str):
-  ser = serial.Serial(
-    port="/dev/ttyUSB0",  # або "COM3" на Windows
-      baudrate=9600,
-      timeout=1  # щоб не блокувався назавжди
-  )
+  def getLogger(self) -> logging.Logger:
+    logging.basicConfig(
+      level=logging.INFO,
+      format='%(asctime)s [%(levelname)s] %(message)s'
+    )
 
-serial_port = find_serial_port(PORT_FILTER)
-print(serial_port)
+    return logging.getLogger(__name__)
+  
+  def find_serial_port(self, filter: str) -> str:
+    ports = serial.tools.list_ports.comports()
+    for port in ports:
+      if port.device.startswith(filter):
+        return port.device
+    return None
+  
+  def read_serial(self, port: str):
+    ser = serial.Serial(port=port, baudrate=9600, timeout=1)
+
+    self.logger.info(f"Connected to {port}")
+
+    try:
+      while True:
+        if ser.in_waiting > 0:
+          line = ser.readline().decode('utf-8', errors='ignore').strip()
+          self.logger.info("Received:", line)
+    except KeyboardInterrupt:
+      self.logger.info("Done")
+    finally:
+      ser.close()
+
+
+def main() -> None:
+  LoraTunning()
+
+if __name__ == "__main__":
+    main()
 
 
   
