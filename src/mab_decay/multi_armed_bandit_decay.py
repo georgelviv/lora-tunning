966cd0c5687bd90073275cad3661965cbd686ff8
Yurii Shkoropad <lvivgeorge@gmail.com>
2025-10-03 12:14:52 +0300

    refactor with base class

---
+++ src/mab_decay/multi_armed_bandit_decay.py
@@ -0,0 +1,7 @@
+from ..models import Action
+from ..mab import MultiArmedBandit
+
+class MultiArmedBanditDecay(MultiArmedBandit):
+  def update(self, action: Action, reward):
+    super().update(action, reward)
+    self.epsilon = max(0.01, self.epsilon * 0.995)
