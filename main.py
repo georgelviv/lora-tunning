66035a33092fe15aea1d8183822a43856bac1534
Yurii Shkoropad <lvivgeorge@gmail.com>
2026-01-24 23:16:12 +0200

    fix: main script

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ load_dotenv(override=True)
 
 PORT_FILTER = os.getenv('PORT_FILTER')
 
-def get_alg(logger: logging.Logger, backend: LoraBase) -> Algorithm:
+def get_alg(backend: LoraBase) -> Algorithm:
   algorithm: Algorithm = MultiArmedBandit()
   results_dir = Path(__file__).parent / "results" / backend.name / algorithm.name
   algorithm.set_results_dir(results_dir)
@@ -21,26 +21,26 @@ def get_alg(logger: logging.Logger, backend: LoraBase) -> Algorithm:
   return algorithm
 
 def get_backend(logger: logging.Logger) -> LoraBase:
-  backend: LoraBase = LoraHardwareModel(logger, PORT_FILTER)
-  # env_model: EnvironmentModel = EnvironmentModel(
-  #   name=f"math-100-meters",
-  #   path_loss_exponent=2.5,
-  #   shadow_sigma_db=3.0,
-  #   sigma_noise_db=2.0,
-  #   distance_m=100,
-  #   hb_m = 1.2,
-  #   hm_m = 1.0,
-  #   area_type=AreaType.SUBURBAN,
-  #   description=f"Suburban 100 meters"
-  # )
-  # backend = LoraHardwareModel(logger, env_model)
+  # backend: LoraBase = LoraHardwareModel(logger, PORT_FILTER)
+  env_model: EnvironmentModel = EnvironmentModel(
+    name=f"math-100-meters",
+    path_loss_exponent=2.5,
+    shadow_sigma_db=3.0,
+    sigma_noise_db=2.0,
+    distance_m=100,
+    hb_m = 1.2,
+    hm_m = 1.0,
+    area_type=AreaType.SUBURBAN,
+    description=f"Suburban 100 meters"
+  )
+  backend = LoraSimulationModel(logger, env_model)
   return backend
 
 
 async def main():
   logger: logging.Logger = getLogger()
   backend: LoraBase = get_backend(logger)
-  alg: Algorithm = get_alg(logger, backend)
+  alg: Algorithm = get_alg(backend)
 
   loraTunning = LoraTunning(logger, backend, alg)
   await loraTunning.run()
